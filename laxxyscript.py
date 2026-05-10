631cbc61b9548232aefec199b9fdcf4d67f0fcbc
gfk3316-beep <gfk3316@gmail.com>
2026-05-10 12:50:06 +0530

    Update ayanislive8.py

--- laxxyscript.py
+++ laxxyscript.py
@@ -20,11 +20,11 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8615481558:AAFvYPrlft9BDAazubXU1qVheXVRLCOTtLo"
+BOT_TOKEN = "8652411411:AAHYzBpYC_hxWjTHg-LjFpeM772_VgU8oWE"
 YML_FILE_PATH = ".gitlab-ci.yml"
 BINARY_FILE_NAME = "soul"
 ATTACK_HISTORY_FILE = "attack_history.json"
-ADMIN_IDS = [7723430318]
+]ADMIN_IDS = [7178871598]
 TARGET_PROJECT_NAME = "soul-worker"  # NEW: Name of project to auto-create
 
 # Conversation states
